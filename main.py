73e069ddf23b6a65149e065386fb7b00eca8f8af
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-01-25 14:27:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from mesClasses import *
 
 
 if __name__ == '__main__':
-    ia_or_not = input("Voulez vous jouer avec une ia ?(y/n)")
-    player_1 = Player(input("player_1 choisis ton pseudo: \n"))
-    if ia_or_not == 'y':
-        player_2 = Ia()
-    else:
-        player_2 = Player(input("player_2 choisis ton pseudo: \n"))
-    start_game = MyGame(player_1, player_2)
+    restart_game = ""
+    while True:
+        ia_or_not = ''
+        player_1 = Player(input("player_1 choisis ton pseudo: \n"))
+        while ia_or_not != 'y' and ia_or_not != 'n':
+            ia_or_not = input("Voulez vous jouer avec une ia ?(y/n)")
+            if ia_or_not == 'y':
+                player_2 = Ia()
+            elif ia_or_not == 'n':
+                player_2 = Player(input("player_2 choisis ton pseudo: \n"))
+        start_game = MyGame(player_1, player_2)
+        restart_game = input("rejouer ?(y/n)")
+        if restart_game == 'n':
+            break
